b41963b79d85c26faeda32e29225c3e8495277ff
Rostislav Belo <rabelousov@gmail.com>
2021-06-19 01:00:09 +0300

    Добавляет генерацию карточек объектов аренды

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
-const isNumber = (value) => typeof n === 'number' && !Number.isNaN(value);
+const AVATAR_NUMBERS = 10;
+
+const TITLE = ['Сдается в аренду', 'Проживание со всеми удобствами', 'Предлагаем для длительного проживания', 'Вы захотите вернуться сюда снова'];
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+const TIMES = ['12:00', '13:00', '14:00'];
+
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const DESCRIPTION = [
+  'Великолепный вариант для комфортного проживания, богатая инфраструктура вокруг',
+  'Апартаменты комфорт-класса для состоятельного гостя: дорого, но всё по-богатому',
+  'Суперквартира недалеко от центра и тусовки филлипинских девушек',
+  'Восхитительные виды из окон и приветливые глухониемые соседи'];
+
+const PHOTOS_ROOT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking';
+
+const PHOTOS = [
+  `${PHOTOS_ROOT}/duonguyen-8LrGtIxxa4w.jpg`,
+  `${PHOTOS_ROOT}/brandon-hoogenboom-SNxQGWxZQi0.jpg`,
+  `${PHOTOS_ROOT}/claire-rendall-b6kAwr1i0Iw.jpg`];
+
+const LOCATION = {
+  LAT_MIN: 35.65000,
+  LAT_MAX: 35.70000,
+  LNG_MIN: 139.70000,
+  LNG_MAX: 139.80000,
+};
+
+const isNumber = (value) => typeof value === 'number' && !Number.isNaN(value) && value >= 0;
 
 const getRandomFraction = (min = 0, max = 0, dec = 0) => {
   if (!(isNumber(min) && isNumber(max) && isNumber(dec))) {
-    throw new Error('Все аргументы должны быть числами');
+    throw new Error('Все аргументы должны быть числами, равными или больше нуля');
   }
 
   const from = Math.min(min, max);
@@ -12,10 +42,70 @@ const getRandomFraction = (min = 0, max = 0, dec = 0) => {
   return Math.round((Math.random() * (to - from) + from) * fraction) / fraction;
 };
 
-getRandomFraction(0, 12, 3);
-
 const getRandomInteger = (min, max) => getRandomFraction(min, max, 0);
 
-getRandomInteger(0, 33);
+const makeUniqueRandomIntegerGenerator = (min, max) => {
+  const previousValues = [];
 
-/* const isNumber = n => typeof n === 'number' && !Number.isNaN(n); */
+  return () => {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      throw new Error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const randomAvatarIdx = makeUniqueRandomIntegerGenerator(1, AVATAR_NUMBERS);
+
+
+const getCard = () => {
+
+  const padLeft = `${randomAvatarIdx()}`.padStart(2, '0');
+
+  const getAvatar = `img/avatars/user${padLeft}.png`;
+
+  const time = getRandomArrayElement(TIMES);
+
+  const lat = getRandomFraction(LOCATION.LAT_MIN, LOCATION.LAT_MAX, 5);
+
+  const lng = getRandomFraction(LOCATION.LNG_MIN, LOCATION.LNG_MAX, 5);
+
+  return {
+    author: {
+      avatar: getAvatar,
+    },
+    offer: {
+      title: getRandomArrayElement(TITLE),
+      address: `location.${lat}, location.${lng}`,
+      price: getRandomInteger(1, 1000000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 20),
+      checkin: time,
+      checkout: time,
+      features: FEATURES.slice(getRandomInteger(0, FEATURES.length - 1)),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: PHOTOS.slice(getRandomInteger(0, PHOTOS.length - 1)),
+    },
+    location: {
+      lat,
+      lng,
+    },
+  };
+};
+
+getCard();
+
+// Ниже проверка - вызов в консоль 10 результатов. Закомментировано, чтобы нпм-тест не ругался;
+
+/* for (let i = 1; i <= 10; i++) {
+  console.log(getCard());
+}
+ */
